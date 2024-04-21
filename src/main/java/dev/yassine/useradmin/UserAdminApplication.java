4e88a1ef7e2d1af50627b3fcadfd1c13a06fcec1
YassOS <el.yassine.azami@gmail.com>
2024-04-21 19:54:53 +0200

    initialize project

---
+++ src/main/java/dev/yassine/useradmin/UserAdminApplication.java
@@ -0,0 +1,13 @@
+package dev.yassine.useradmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/yassine/useradmin/UserAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yassine.useradmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
