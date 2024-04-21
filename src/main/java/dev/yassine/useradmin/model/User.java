b966cbd7860f6c69acc38f2d3751687782b5e3cd
YassOS <el.yassine.azami@gmail.com>
2024-04-21 20:35:52 +0200

    feat: added user model, repo & controller, updated application.properties to add postgres connection conf

---
+++ src/main/java/dev/yassine/useradmin/model/User.java
@@ -0,0 +1,66 @@
+package dev.yassine.useradmin.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "app_user")
+public class User {
+	
+	@Id
+	@SequenceGenerator(
+			name = "user_sequence",
+			sequenceName = "user_sequence",
+			allocationSize = 1
+	)
+	@GeneratedValue(
+			strategy = GenerationType.SEQUENCE,
+			generator = "user_sequence"
+	)
+	private Long id;
+	
+	private String username;
+	
+	private String name;
+
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
